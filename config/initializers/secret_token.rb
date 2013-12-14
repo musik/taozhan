72241a627af74204a92e0194b6cdcc5d225225d9
musik <cgicn@qq.com>
2013-12-14 19:56:50 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Taozhan::Application.config.secret_key_base = 'db9d9d5455d709a0ad74ed3cb59ebd6b4156273f39b719e5db8b172ef9252ba1ce6722880d904eddc5c0a951c719ac30f06741086f79095271e2d6447e3c38cf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Taozhan::Application.config.session_store :cookie_store, key: '_taozhan_session'
